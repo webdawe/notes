14f68b3e52546bf1bfc3d850600b32b24a8e9d9a
Anil Paul <anilnpaul@gmail.com>
2025-03-07 10:01:56 +1100

    clean up

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAN0DekIRVZgSpVEhH54uHVebwpiBZZSNs",
-  authDomain: "notes-bcffc.firebaseapp.com",
-  projectId: "notes-bcffc",
-  storageBucket: "notes-bcffc.firebasestorage.app",
-  messagingSenderId: "94217443311",
-  appId: "1:94217443311:web:68221ba139f0b4902ac81a",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
